4fb4a870c1e11889f4c9b9ad48828420ec1676ed
maharshi-me <maharshi3781@gmail.com>
2024-06-09 00:53:20 +0530

    Bug fix

--- src/utils/functions/getShortRupeesString.js
+++ src/utils/functions/getShortRupeesString.js
@@ -4,10 +4,10 @@ const getShortRupeesString = n => {
     return String(num)
   }
   else if (num < 100000) {
-    return String((num / 1000).toFixed(1)) + ' ¯k'
+    return String((num / 1000).toFixed(1)) + 'K'
   }
 
-  return String((num / 100000).toFixed(1)) + ' L'
+  return String((num / 100000).toFixed(1)) + 'L'
 }
 
 export default getShortRupeesString
