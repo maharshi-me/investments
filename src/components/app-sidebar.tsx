ebe95387138e8f4a1b94027550a4e029260de9fe
maharshi-me <maharshi3781@gmail.com>
2025-02-16 12:32:19 +0530

    fix route

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -7,20 +7,14 @@ import {
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarRail,
 } from "@/components/ui/sidebar"
 
 // This is sample data.
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com"
-  },
   navMain: [
     {
       title: "Dashboard",
@@ -51,9 +45,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarContent>
         <NavMain items={data.navMain} />
       </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   )
